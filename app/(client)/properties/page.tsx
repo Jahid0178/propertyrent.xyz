b356df2565e5678fac4dc54bfaaaf1865664f24b
Zahidul Haque <zahidul.haque767@gmail.com>
2024-06-12 04:00:52 +0600

    feat: add properties page

---
+++ app/(client)/properties/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PropertiesPage = () => {
+  return (
+    <section>
+      <div className="container">
+        <p>All Properties</p>
+      </div>
+    </section>
+  );
+};
+
+export default PropertiesPage;
