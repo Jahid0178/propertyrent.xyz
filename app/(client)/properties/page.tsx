5315d7b14df65ba2b1d8babd99b11eb635e77cd9
Zahidul Haque <zahidul.haque767@gmail.com>
2024-06-15 04:49:48 +0600

    Implement properties page with demo data feat: add property listing topbar component feat: implement property demo data with property card chore(data): add few property demo data

--- app/(client)/properties/page.tsx
+++ app/(client)/properties/page.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import PropertyCard from "@/components/Cards/PropertyCard";
+import PropertyListingTopbar from "@/components/PropertyListingTopbar/PropertyListingTopbar";
+import { getAllProperties } from "@/lib/actions/property.action";
 
-const PropertiesPage = () => {
+const PropertiesPage = async () => {
+  const properties = await getAllProperties();
+  console.log("properties", properties);
   return (
     <section>
       <div className="container">
-        <p>All Properties</p>
+        <div className="mb-6">
+          <PropertyListingTopbar properties={properties} />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          {properties.map((property: any) => (
+            <PropertyCard key={property.id} propertyData={property} />
+          ))}
+        </div>
       </div>
     </section>
   );
