f8cf05d12080b98902955a5626c3571d25012379
Zahidul Haque <zahidul.haque767@gmail.com>
2024-06-16 22:23:55 +0600

    feat: add user authentication form for user login and register

---
+++ components/Forms/UserAuthenticationForm.tsx
@@ -0,0 +1,184 @@
+"use client";
+
+import React from "react";
+import { z } from "zod";
+import { Input } from "../ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+import { FaUser } from "react-icons/fa";
+
+// login form validation schema
+const loginFormSchema = z.object({
+  phoneNumber: z.string().trim().min(3, "Email is required"),
+  password: z.string().trim().min(1, "Password is required"),
+});
+// register form validation schema
+const registerFormSchema = z.object({
+  fullName: z.string().trim().min(3, "Name is required"),
+  phoneNumber: z.string().trim().min(3, "Email is required"),
+  password: z.string().trim().min(1, "Password is required"),
+});
+
+const UserAuthenticationForm = () => {
+  // login form
+  const loginForm = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      phoneNumber: "",
+      password: "",
+    },
+  });
+
+  // register form
+  const registerForm = useForm<z.infer<typeof registerFormSchema>>({
+    resolver: zodResolver(registerFormSchema),
+    defaultValues: {
+      fullName: "",
+      phoneNumber: "",
+      password: "",
+    },
+  });
+
+  // onsubmit handler for form
+  const onSubmit = (data: z.infer<typeof loginFormSchema>) => {
+    console.log("user_auth_form_data", data);
+    loginForm.reset();
+    registerForm.reset();
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline" size="icon">
+          <FaUser />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <Tabs defaultValue="login">
+          <TabsList className="grid grid-cols-2 w-full">
+            <TabsTrigger value="login">Login</TabsTrigger>
+            <TabsTrigger value="register">Register</TabsTrigger>
+          </TabsList>
+          <TabsContent value="login">
+            <Form {...loginForm}>
+              <form
+                onSubmit={loginForm.handleSubmit(onSubmit)}
+                className="space-y-6"
+              >
+                <FormField
+                  control={loginForm.control}
+                  name="phoneNumber"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Phone Number:</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter your phone number"
+                          {...field}
+                          type="text"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={loginForm.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password:</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter your password"
+                          {...field}
+                          type="password"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Login</Button>
+              </form>
+            </Form>
+          </TabsContent>
+          <TabsContent value="register">
+            <Form {...registerForm}>
+              <form
+                onSubmit={registerForm.handleSubmit(onSubmit)}
+                className="space-y-6"
+              >
+                <FormField
+                  control={registerForm.control}
+                  name="fullName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Full Name:</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter your full name"
+                          {...field}
+                          type="text"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={registerForm.control}
+                  name="phoneNumber"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Phone Number:</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter your phone number"
+                          {...field}
+                          type="text"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={registerForm.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password:</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter your password"
+                          {...field}
+                          type="password"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Register</Button>
+              </form>
+            </Form>
+          </TabsContent>
+        </Tabs>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default UserAuthenticationForm;
