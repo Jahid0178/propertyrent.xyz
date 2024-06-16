8a6250eab34d68ecd45b1adb5d324e14ee47dde5
Zahidul Haque <zahidul.haque767@gmail.com>
2024-06-16 06:28:32 +0600

    feat: add property image gallery component

---
+++ components/Sliders/PropertyImageGallery.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+interface IPropertyImageGallery {
+  gallery: string[];
+}
+
+const PropertyImageGallery = ({ gallery }: IPropertyImageGallery) => {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  return (
+    <div>
+      <Swiper
+        className="property-image-gallery-slider"
+        spaceBetween={20}
+        navigation={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        thumbs={{ swiper: thumbsSwiper }}
+      >
+        {gallery.map((image, ind) => (
+          <SwiperSlide key={ind}>
+            <Image
+              src={image}
+              alt="property image"
+              width={1280}
+              height={500}
+              className="w-full h-full"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <Swiper
+        // @ts-ignore
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView={8}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="property-gallery-thumbs-slider"
+      >
+        {gallery.map((image, ind) => (
+          <SwiperSlide key={ind}>
+            <Image
+              src={image}
+              alt="property image"
+              width={1280}
+              height={500}
+              className="w-full h-full"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default PropertyImageGallery;
