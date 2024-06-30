a8a002c0d27c32f0e087087905078e2abe744e14
Zahidul Haque <zahidul.haque767@gmail.com>
2024-07-01 03:08:52 +0600

    feat: add protech dashboard and store user in auth store

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import isAuthenticated from "../utils/isAuthenticated";
 import "./globals.css";
 // Import Swiper styles
 import "swiper/css";
+import InitializeStore from "./InitializeStore";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,14 +14,17 @@ export const metadata: Metadata = {
     "PropertyRent.xyz is a platform that connects property owners with tenants.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const data = await isAuthenticated({ noRedirect: true });
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <InitializeStore user={data?.user?.user}>
+        <body className={inter.className}>{children}</body>
+      </InitializeStore>
     </html>
   );
 }
