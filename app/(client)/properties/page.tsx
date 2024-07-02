2c6b1dfa1073ba5747f47394021ba8d5cc391793
Zahidul Haque <zahidul.haque767@gmail.com>
2024-07-03 02:20:19 +0600

    feat: add get all property handler for get all property info

--- app/(client)/properties/page.tsx
+++ app/(client)/properties/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropertyCard from "@/components/Cards/PropertyCard";
 import PropertyListingTopbar from "@/components/PropertyListingTopbar/PropertyListingTopbar";
-import { getAllProperties } from "@/lib/actions/property.action";
+import { getAllPropertyListings } from "@/lib/actions/property.action";
 
 type PropertiesPageProps = {
   searchParams: any;
@@ -9,7 +9,7 @@ type PropertiesPageProps = {
 
 const PropertiesPage = async ({ searchParams }: PropertiesPageProps) => {
   const params = searchParams;
-  const properties = await getAllProperties(params.search);
+  const { properties } = await getAllPropertyListings();
   return (
     <section>
       <div className="container">
@@ -18,7 +18,7 @@ const PropertiesPage = async ({ searchParams }: PropertiesPageProps) => {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
           {properties.map((property: any) => (
-            <PropertyCard key={property.id} propertyData={property} />
+            <PropertyCard key={property._id} propertyData={property} />
           ))}
         </div>
       </div>
