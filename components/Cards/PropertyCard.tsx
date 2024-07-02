428559579442fc8ea7ff138c60957da61acb489c
Zahidul Haque <zahidul.haque767@gmail.com>
2024-07-03 02:21:01 +0600

    feat: showing property card data based on get all property listing data

--- components/Cards/PropertyCard.tsx
+++ components/Cards/PropertyCard.tsx
@@ -15,28 +15,34 @@ interface IPropertyCard {
   propertyData: PropertyProps;
 }
 
-const PropertyCard = ({ propertyData }: IPropertyCard) => {
+const PropertyCard = ({ propertyData }: { propertyData: PropertyProps }) => {
   const {
-    id,
-    name,
-    location,
-    price,
-    image,
-    bathrooms,
-    bedrooms,
-    currency,
-    squareFeet,
+    _id,
+    images,
     listingType,
+    title,
+    price,
+    currency,
+    propertyDetails: {
+      propertyFeatures: {
+        propertySize,
+        propertySizeUnit,
+        numberOfBedrooms,
+        numberOfBathrooms,
+      },
+    },
+    address: { city },
   } = propertyData;
-  const imageURL = image?.url ? image?.url : "/images/placeholder.png";
+
+  const imageURL = images[0]?.url ? images[0]?.url : "/images/placeholder.png";
   return (
-    <Link href={`/properties/${id}`}>
+    <Link href={`/properties/${_id}`}>
       <Card className="overflow-hidden">
         <CardHeader className="px-0 pt-0 pb-6 relative">
           <Badge className="absolute top-3 left-3">{listingType}</Badge>
           <Image
             src={imageURL}
-            alt={name}
+            alt={title}
             width={800}
             height={800}
             className="!m-0 h-52"
@@ -45,17 +51,17 @@ const PropertyCard = ({ propertyData }: IPropertyCard) => {
           />
         </CardHeader>
         <CardContent>
-          <CardTitle className="mb-3">{name}</CardTitle>
+          <CardTitle className="mb-3">{title}</CardTitle>
           <div className="space-y-2">
             <div className="flex justify-between items-center gap-2">
               <p className="text-sm flex gap-1 items-center">
-                <RxSize size={18} /> {squareFeet} Sq.ft
+                <RxSize size={18} /> {propertySize} {propertySizeUnit}
               </p>
               <p className="text-sm flex gap-1 items-center">
-                <IoBedOutline size={18} /> {bedrooms} Beds
+                <IoBedOutline size={18} /> {numberOfBedrooms} Beds
               </p>
               <p className="text-sm flex gap-1 items-center">
-                <TbBath size={18} /> {bathrooms} Baths
+                <TbBath size={18} /> {numberOfBathrooms} Baths
               </p>
             </div>
             <div className="flex justify-between items-center gap-2">
@@ -64,7 +70,7 @@ const PropertyCard = ({ propertyData }: IPropertyCard) => {
                 {currency}
               </p>
               <p className="text-sm flex gap-1 items-center">
-                <FaLocationArrow size={18} /> {location}
+                <FaLocationArrow size={18} /> {city}
               </p>
             </div>
           </div>
