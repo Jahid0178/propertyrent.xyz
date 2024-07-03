0cab06dbe72ee46c4a0c3e522613b3463d8b6826
Zahidul Haque <zahidul.haque767@gmail.com>
2024-07-03 07:13:36 +0600

    feat: add credit package card and data

--- app/(dashboard)/user/dashboard/buy-credit/page.tsx
+++ app/(dashboard)/user/dashboard/buy-credit/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import PackageCard from "@/components/Cards/PackageCard";
+import { creditPackages } from "@/data/data";
+import { IPackage } from "@/typescript/interface";
 
 const BuyCreditPage = () => {
-  return <div>Buy Credit Page</div>;
+  return (
+    <section>
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {creditPackages.map((creditPackage: IPackage) => (
+            <PackageCard
+              key={creditPackage._id}
+              creditPackage={creditPackage}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BuyCreditPage;
