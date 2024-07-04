b00429fa75717e19ede0b649c4d0dd19f03e3bd2
Zahidul Haque <zahidul.haque767@gmail.com>
2024-07-04 06:15:22 +0600

    feat: showing user listed property in my property page

--- components/Sidebars/DashboardSidebar/DashboardSidebar.tsx
+++ components/Sidebars/DashboardSidebar/DashboardSidebar.tsx
@@ -1,15 +1,25 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import authStore from "@/store/authStore";
 import { dashboardSidebarLinks } from "@/data/data";
 
 const DashboardSidebar = () => {
+  const { user } = authStore((state) => state);
+
   return (
     <aside className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700">
       <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
         <ul className="space-y-2 font-medium">
           {dashboardSidebarLinks.map((link) => (
             <li key={link.id} className="hover:bg-orange-50">
-              <Link href={link.href} className="flex gap-2 items-center p-2">
+              <Link
+                href={
+                  link.id === 3 ? `${link.href}?id=${user?._id}` : link.href
+                }
+                className="flex gap-2 items-center p-2"
+              >
                 {link.icon({ size: 20 })} {link.name}
               </Link>
             </li>
---
+++ components/Tables/PropertyListingTable.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { FaRegEdit, FaRegTrashAlt } from "react-icons/fa";
+import formatNumberWithCommas from "@/utils/formatNumberWithCommas";
+import { Badge } from "../ui/badge";
+
+const PropertyListingTable = ({ properties }: any) => {
+  return (
+    <Table className="border">
+      <TableCaption>
+        A list of your {properties.length > 1 ? "properties" : "property"}.
+      </TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Image</TableHead>
+          <TableHead>Property ID</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead className="text-right">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {properties.map((property: any) => (
+          <TableRow key={property._id}>
+            <TableCell>
+              <Image
+                src={property?.images[0]?.url}
+                alt={property.title}
+                width={100}
+                height={100}
+                className="rounded-md"
+              />
+            </TableCell>
+            <TableCell className="font-medium">{property._id}</TableCell>
+            <TableCell>{property.title}</TableCell>
+            <TableCell>
+              {formatNumberWithCommas(property.price)} {property.currency}
+            </TableCell>
+            <TableCell>
+              <Badge>Active</Badge>
+            </TableCell>
+            <TableCell className="text-right space-x-2">
+              <Button size="icon">
+                <FaRegEdit size={16} />
+              </Button>
+              <Button size="icon" variant="destructive">
+                <FaRegTrashAlt size={16} />
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default PropertyListingTable;
