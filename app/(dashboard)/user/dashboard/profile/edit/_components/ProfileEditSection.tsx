3edcd16119425926607ddfc5ca09fd6f21459ec5
Jahid0178 <jahidulhaque767@gmail.com>
2024-07-06 05:00:04 +0600

    feat: add address field in edit page
    feat: implement user update profile functionality using api

---
+++ app/(dashboard)/user/dashboard/profile/edit/_components/ProfileEditSection.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProfileEditForm from "./ProfileEditForm";
+
+const ProfileEditSection = () => {
+  return (
+    <section>
+      <div className="container">
+        <ProfileEditForm />
+      </div>
+    </section>
+  );
+};
+
+export default ProfileEditSection;
