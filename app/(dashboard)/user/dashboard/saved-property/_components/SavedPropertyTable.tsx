462d77ff83616ddfe6de5c0ae062de3220476221
Jahid0178 <jahidulhaque767@gmail.com>
2024-07-07 20:19:19 +0600

    feat: showing user saved property table in saved property page

---
+++ app/(dashboard)/user/dashboard/saved-property/_components/SavedPropertyTable.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { FaRegTrashAlt, FaLink } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+import authStore from "@/store/authStore";
+import { handleGetSavedPropertyByUserId } from "@/lib/actions/property.action";
+import formatNumberWithCommas from "@/utils/formatNumberWithCommas";
+import Image from "next/image";
+import Link from "next/link";
+import moment from "moment";
+
+const SavedPropertyTable = () => {
+  const [savedProperties, setSavedProperties] = useState([]);
+  const { user } = authStore((state) => state);
+
+  useEffect(() => {
+    const getSavedPropertyByUserId = async () => {
+      const savedProperties = await handleGetSavedPropertyByUserId(user?._id);
+      setSavedProperties(savedProperties);
+    };
+    getSavedPropertyByUserId();
+  }, []);
+
+  return (
+    <Table className="border">
+      <TableCaption>A list of your saved properties</TableCaption>
+      <TableHeader className="border-b">
+        <TableHead className="w-[100px]">Image</TableHead>
+        <TableHead>Property ID</TableHead>
+        <TableHead>Name</TableHead>
+        <TableHead>Price</TableHead>
+        <TableHead>Saved at</TableHead>
+        <TableHead className="text-right">Action</TableHead>
+      </TableHeader>
+      <TableBody>
+        {savedProperties.map((savedProperty, ind) => {
+          const {
+            _id,
+            createdAt,
+            propertyId: {
+              title,
+              images,
+              price,
+              propertyType,
+              listingType,
+              _id: propertyId,
+            },
+          } = savedProperty;
+          const formattedPrice = formatNumberWithCommas(price);
+          const formattedTime = moment(createdAt).format("LL");
+          return (
+            <TableRow key={ind}>
+              <TableCell className="font-medium">
+                <Image
+                  // @ts-ignore
+                  src={images?.[0]?.url || "/images/placeholder.png"}
+                  alt={title}
+                  width={100}
+                  height={100}
+                  className="rounded-md"
+                />
+              </TableCell>
+              <TableCell>{_id}</TableCell>
+              <TableCell>{title}</TableCell>
+              <TableCell>{formattedPrice}</TableCell>
+              <TableCell>{formattedTime}</TableCell>
+              <TableCell className="text-right space-x-4">
+                <Button variant="outline" size="sm" asChild>
+                  <Link href={`/properties/${propertyId}`}>
+                    <FaLink />
+                  </Link>
+                </Button>
+                <Button variant="destructive" size="sm">
+                  <FaRegTrashAlt />
+                </Button>
+              </TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default SavedPropertyTable;
