22ea8bfc6397aa802643c0cd59a69dc875f18526
Jahid0178 <jahidulhaque767@gmail.com>
2024-07-10 18:59:20 +0600

    feat: add dashboard stats card icons

--- components/Cards/DashboardMetricCard.tsx
+++ components/Cards/DashboardMetricCard.tsx
@@ -4,13 +4,19 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 type PropertyStatsCardProps = {
   title: string;
   count: number;
+  icon: any;
 };
 
-const DashboardMetricCard = ({ title, count }: PropertyStatsCardProps) => {
+const DashboardMetricCard = ({
+  title,
+  count,
+  icon,
+}: PropertyStatsCardProps) => {
   return (
     <Card>
-      <CardHeader>
+      <CardHeader className="flex-row justify-between items-center space-y-0">
         <CardTitle>{title}</CardTitle>
+        <span>{icon}</span>
       </CardHeader>
       <CardContent>
         <p className="text-3xl font-semibold">{count}</p>
