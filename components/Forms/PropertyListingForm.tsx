75f2f19c83106aafad2d30576d5ea98b80df95bc
Jahid0178 <jahidulhaque767@gmail.com>
2024-07-11 23:51:27 +0600

    feat: add onCoordinatesChange function to get lat lng value from map

--- components/Forms/PropertyListingForm.tsx
+++ components/Forms/PropertyListingForm.tsx
@@ -39,196 +39,19 @@ import {
   updatePropertyListing,
 } from "@/lib/actions/property.action";
 import { PropertyProps } from "@/typescript/interface";
+import { PiWarningCircleBold } from "react-icons/pi";
 import { toast } from "react-toastify";
+import ListingMap from "@/app/(dashboard)/user/dashboard/add-property/_components/ListingMap";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
+import { DEFAULT_MAP_LAT, DEFAULT_MAP_LNG } from "@/constant/constant";
+import { propertyListingFormValidation } from "@/validation/validation";
 
-const FormSchema = z.object({
-  title: z
-    .string({
-      required_error: "Title is required",
-      invalid_type_error: "Title must be a string",
-    })
-    .min(3, { message: "Title must be at least 3 characters" })
-    .trim(),
-  description: z
-    .string({
-      required_error: "Description is required",
-      invalid_type_error: "Description must be a string",
-    })
-    .min(20, { message: "Description must be at least 20 characters" })
-    .trim(),
-  propertyType: z.string().nonempty("Property Type is required"),
-  listingType: z.string().nonempty("Listing Type is required"),
-  currency: z.string().min(1, { message: "Currency is required" }),
-  images: z.any(),
-  address: z.object({
-    street: z
-      .string({
-        required_error: "Street is required",
-        invalid_type_error: "Street must be a string",
-      })
-      .min(3, { message: "Street must be at least 3 characters" }),
-    city: z
-      .string({
-        required_error: "City is required",
-        invalid_type_error: "City must be a string",
-      })
-      .min(3, { message: "City must be at least 3 characters" }),
-    country: z
-      .string({
-        required_error: "Country is required",
-        invalid_type_error: "Country must be a string",
-      })
-      .min(3, { message: "Country must be at least 3 characters" }),
-    zipCode: z
-      .string({
-        required_error: "Zip Code is required",
-        invalid_type_error: "Zip Code must be a string",
-      })
-      .min(3, { message: "Zip Code must be at least 3 characters" }),
-  }),
-  price: z
-    .string({
-      required_error: "Price is required",
-      invalid_type_error: "Price must be a string",
-    })
-    .min(3, { message: "Price must be at least 3 characters" }),
-  propertyDetails: z.object({
-    propertyFeatures: z.object({
-      propertySize: z
-        .string({
-          required_error: "Property Size is required",
-          invalid_type_error: "Property Size must be a string",
-        })
-        .min(3, { message: "Property Size must be at least 3 characters" }),
-      propertySizeUnit: z
-        .string({
-          required_error: "Property Size Unit is required",
-          invalid_type_error: "Property Size must be a string",
-        })
-        .nonempty("Property Size Unit is required"),
-      numberOfBedrooms: z
-        .string({
-          required_error: "Number of Bedrooms is required",
-          invalid_type_error: "Number of Bedrooms must be a string",
-        })
-        .nonempty("Number of Bedrooms is required"),
-      numberOfBathrooms: z
-        .string({
-          required_error: "Number of Bathrooms is required",
-          invalid_type_error: "Number of Bathrooms must be a string",
-        })
-        .nonempty("Number of Bathrooms is required"),
-      numberOfDiningrooms: z
-        .string({
-          required_error: "Number of Diningrooms is required",
-          invalid_type_error: "Number of Diningrooms must be a string",
-        })
-        .nonempty("Number of Diningrooms is required"),
-      numberOfGarage: z
-        .string({
-          required_error: "Number of Garage is required",
-          invalid_type_error: "Number of Garage must be a string",
-        })
-        .nonempty("Number of Garage is required"),
-      numberOfBalconies: z
-        .string({
-          required_error: "Number of Balconies is required",
-          invalid_type_error: "Number of Balconies must be a string",
-        })
-        .nonempty("Number of Balconies is required"),
-      renovation: z
-        .string({
-          required_error: "Renovation is required",
-          invalid_type_error: "Renovation must be a string",
-        })
-        .nonempty("Renovation is required"),
-      yearBuilt: z
-        .string({
-          required_error: "Year Built is required",
-          invalid_type_error: "Year Built must be a string",
-        })
-        .nonempty("Year Built is required"),
-    }),
-    propertyUtilities: z.object({
-      gas: z
-        .string({
-          required_error: "Gas is required",
-          invalid_type_error: "Gas must be a string",
-        })
-        .nonempty("Gas is required"),
-      electricity: z
-        .string({
-          required_error: "Electricity is required",
-          invalid_type_error: "Electricity must be a string",
-        })
-        .nonempty("Electricity is required"),
-      internet: z
-        .string({
-          required_error: "Internet is required",
-          invalid_type_error: "Internet must be a string",
-        })
-        .nonempty("Internet is required"),
-      water: z
-        .string({
-          required_error: "Water is required",
-          invalid_type_error: "Water must be a string",
-        })
-        .nonempty("Water is required"),
-    }),
-    outdoorFeatures: z.object({
-      garden: z
-        .string({
-          required_error: "Garden is required",
-          invalid_type_error: "Garden must be a string",
-        })
-        .nonempty("Garden is required"),
-      pool: z
-        .string({
-          required_error: "Pool is required",
-          invalid_type_error: "Pool must be a string",
-        })
-        .nonempty("Pool is required"),
-      playground: z
-        .string({
-          required_error: "Playground is required",
-          invalid_type_error: "Playground must be a string",
-        })
-        .nonempty("Playground is required"),
-      fencing: z
-        .string({
-          required_error: "Fencing is required",
-          invalid_type_error: "Fencing must be a string",
-        })
-        .nonempty("Fencing is required"),
-    }),
-    nearby: z.object({
-      school: z
-        .string({
-          required_error: "Schools is required",
-          invalid_type_error: "Schools must be a string",
-        })
-        .min(1, { message: "Schools is required" }),
-      hospital: z
-        .string({
-          required_error: "Hospitals is required",
-          invalid_type_error: "Hospitals must be a string",
-        })
-        .min(1, { message: "Hospitals is required" }),
-      shoppingCenter: z
-        .string({
-          required_error: "Shopping Centers is required",
-          invalid_type_error: "Shopping Centers must be a string",
-        })
-        .min(1, { message: "Shopping Centers is required" }),
-      publicTransport: z
-        .string({
-          required_error: "Public Transport is required",
-          invalid_type_error: "Public Transport must be a string",
-        })
-        .min(1, { message: "Public Transport is required" }),
-    }),
-  }),
-});
+const FormSchema = z.object(propertyListingFormValidation);
 
 interface PropertyListingFormProps {
   property?: any;
@@ -239,7 +62,7 @@ const PropertyListingForm = ({
   property,
   formType = "create",
 }: PropertyListingFormProps) => {
-  const form = useForm<z.infer<typeof FormSchema>>({
+  const form = useForm({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       title: property?.title || "",
@@ -303,6 +126,10 @@ const PropertyListingForm = ({
             property?.propertyDetails?.nearby?.publicTransport || "",
         },
       },
+      coordinates: {
+        lat: property?.coordinates?.lat || DEFAULT_MAP_LAT,
+        lng: property?.coordinates?.lng || DEFAULT_MAP_LNG,
+      },
     },
   });
 
@@ -310,13 +137,6 @@ const PropertyListingForm = ({
     try {
       const formData = new FormData();
 
-      // Append files
-      // if(data.images) {
-      //   data.images.forEach((image: any) => {
-      //     console.log(image)
-      //     formData.append("images", image);
-      //   })
-      // }
       if (data.images && data.images.length > 0) {
         for (let i = 0; i < data.images.length; i++) {
           formData.append("images", data.images[i]);
@@ -347,6 +167,11 @@ const PropertyListingForm = ({
       console.error("Error uploading image:", error);
     }
   };
+
+  const onCoordinatesChange = (lat: number, lng: number) => {
+    form.setValue("coordinates", { lat, lng });
+  };
+
   return (
     <Form {...form}>
       <form
@@ -1110,6 +935,22 @@ const PropertyListingForm = ({
               )}
             />
           </div>
+        </div>
+        <div className="bg-gray-100 dark:bg-gray-900 rounded-md p-4 space-y-4">
+          <h3 className="text-lg font-medium flex items-center gap-2">
+            Maps{" "}
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <PiWarningCircleBold size={16} />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Adjust marker on the map to set your property location</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </h3>
+          <ListingMap onCoordinatesChange={onCoordinatesChange} />
         </div>
         <Button type="submit" size="lg">
           {formType === "create" ? "Add Property" : "Update Property"}
