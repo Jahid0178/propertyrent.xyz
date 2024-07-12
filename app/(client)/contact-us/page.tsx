2b14cce81d5f1b05b45e9c9dba0e3fdde4f38370
Jahid0178 <jahidulhaque767@gmail.com>
2024-07-13 04:38:44 +0600

    feat: add contact us page with google map

---
+++ app/(client)/contact-us/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ContactUsSection from "./_components/ContactUsSection";
+
+const ContactUsPage = () => {
+  return <ContactUsSection />;
+};
+
+export default ContactUsPage;
