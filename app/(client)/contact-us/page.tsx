231846adced9a95d29738a3cf279fdc1f761c26b
Jahid0178 <jahidulhaque767@gmail.com>
2024-07-29 04:52:40 +0600

    feat: add meta data in contact us page

--- app/(client)/contact-us/page.tsx
+++ app/(client)/contact-us/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Metadata } from "next";
 import ContactUsSection from "./_components/ContactUsSection";
+
+export const metadata: Metadata = {
+  title: "PropertyRent.xyz | Contact Us",
+  description: "Contact us for any inquiries or questions. We're here to help!",
+};
 
 const ContactUsPage = () => {
   return <ContactUsSection />;
