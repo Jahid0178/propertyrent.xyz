9b86c6ea14e12461780e04db7d6777f61920d05c
Jahid0178 <jahidulhaque767@gmail.com>
2024-08-15 12:33:52 +0600

    feat: add floor field and conditionally render gender field

--- components/Forms/PropertyListingForm/PropertyFeatures.tsx
+++ components/Forms/PropertyListingForm/PropertyFeatures.tsx
@@ -7,8 +7,23 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
-const PropertyFeatures = ({ form }: { form: any }) => {
+interface PropertyFeaturesProps {
+  form: any;
+  propertyCategory?: string;
+}
+
+const PropertyFeatures = ({
+  form,
+  propertyCategory,
+}: PropertyFeaturesProps) => {
   return (
     <div className="bg-gray-100 dark:bg-gray-900 rounded-md p-4 space-y-4">
       <h3 className="text-lg font-medium">Property Features</h3>
@@ -107,7 +122,72 @@ const PropertyFeatures = ({ form }: { form: any }) => {
             </FormItem>
           )}
         />
+        <FormField
+          control={form.control}
+          name="propertyDetails.propertyFeatures.numberOfFloors"
+          render={({ field }) => (
+            <FormItem className="flex-1">
+              <FormLabel>Floor No</FormLabel>
+              <FormControl>
+                <Input
+                  type="number"
+                  className="bg-white resize-none"
+                  placeholder="Enter number of garage"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
       </div>
+      {["bachelor", "sublet"].includes(propertyCategory ?? "") && (
+        <div className="flex flex-col md:flex-row gap-4">
+          <FormField
+            control={form.control}
+            name="propertyDetails.propertyFeatures.gender"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>Gender</FormLabel>
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className="bg-white">
+                      <SelectValue placeholder="Select Gender" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="Male">Male</SelectItem>
+                      <SelectItem value="Female">Female</SelectItem>
+                      <SelectItem value="Both">Both</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* <FormField
+          control={form.control}
+          name="propertyDetails.propertyFeatures.numberOfFloors"
+          render={({ field }) => (
+            <FormItem className="flex-1">
+              <FormLabel>Floor No</FormLabel>
+              <FormControl>
+                <Input
+                  type="number"
+                  className="bg-white resize-none"
+                  placeholder="Enter number of garage"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        /> */}
+        </div>
+      )}
     </div>
   );
 };
