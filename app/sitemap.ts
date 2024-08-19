896b5cbf17c0173a8fd2b5901034e41c2d7c987e
Jahid0178 <jahidulhaque767@gmail.com>
2024-08-19 23:07:03 +0600

    chore(env): update sitemap URLs to use environment variables
    - Replaced hardcoded URLs with environment variables for better flexibility and configuration management.
    - Updated sitemap entries to dynamically use `NEXT_PUBLIC_FRONTEND_URL`.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "http://localhost:3000",
+      url: `${process.env.NEXT_PUBLIC_FRONTEND_URL}`,
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
     },
     {
-      url: "http://localhost:3000/properties",
+      url: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/properties`,
       lastModified: new Date(),
       changeFrequency: "hourly",
       priority: 1,
     },
     {
-      url: "http://localhost:3000/about-us",
+      url: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/about-us`,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.5,
