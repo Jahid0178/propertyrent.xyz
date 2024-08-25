3cb516d1c4b7f4873f27455bac70c067d17e72e1
Jahid0178 <jahidulhaque767@gmail.com>
2024-08-26 01:49:56 +0600

    feat: add expiresAt table column and table head in PropertyListingTable component

--- components/Tables/PropertyListingTable.tsx
+++ components/Tables/PropertyListingTable.tsx
@@ -14,8 +14,14 @@ import { FaRegEdit, FaRegTrashAlt } from "react-icons/fa";
 import formatNumberWithCommas from "@/utils/formatNumberWithCommas";
 import { Badge } from "../ui/badge";
 import Link from "next/link";
+import { PropertyProps } from "@/typescript/interface";
+import moment from "moment";
 
-const PropertyListingTable = ({ properties }: any) => {
+interface PropertyListingTableProps {
+  properties: PropertyProps[];
+}
+
+const PropertyListingTable = ({ properties }: PropertyListingTableProps) => {
   return (
     <Table className="border dark:border-gray-800">
       <TableCaption>
@@ -26,43 +32,52 @@ const PropertyListingTable = ({ properties }: any) => {
           <TableHead className="w-[100px]">Image</TableHead>
           <TableHead>Property ID</TableHead>
           <TableHead>Name</TableHead>
+          <TableHead>Expired Date</TableHead>
           <TableHead>Price</TableHead>
           <TableHead>Status</TableHead>
           <TableHead className="text-right">Actions</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {properties.map((property: any) => (
-          <TableRow key={property._id}>
-            <TableCell>
-              <Image
-                src={property?.images[0]?.url || "/images/placeholder.png"}
-                alt={property.title}
-                width={100}
-                height={100}
-                className="rounded-md"
-              />
-            </TableCell>
-            <TableCell className="font-medium">{property._id}</TableCell>
-            <TableCell>{property.title}</TableCell>
-            <TableCell>
-              {formatNumberWithCommas(property.price)} {property.currency}
-            </TableCell>
-            <TableCell>
-              <Badge>Active</Badge>
-            </TableCell>
-            <TableCell className="text-right space-x-2">
-              <Button size="icon" asChild>
-                <Link href={`/user/dashboard/my-property/edit/${property._id}`}>
-                  <FaRegEdit size={16} />
-                </Link>
-              </Button>
-              <Button size="icon" variant="destructive">
-                <FaRegTrashAlt size={16} />
-              </Button>
-            </TableCell>
-          </TableRow>
-        ))}
+        {properties.map((property: any) => {
+          const formatExpiredDate = moment(property.expiresAt).format("ll");
+          return (
+            <TableRow key={property._id}>
+              <TableCell>
+                <Image
+                  src={property?.images[0]?.url || "/images/placeholder.png"}
+                  alt={property.title}
+                  width={100}
+                  height={100}
+                  className="rounded-md"
+                />
+              </TableCell>
+              <TableCell className="font-medium">{property._id}</TableCell>
+              <TableCell>{property.title}</TableCell>
+              <TableCell>
+                {property.expiresAt ? formatExpiredDate : "-"}
+              </TableCell>
+              <TableCell>
+                {formatNumberWithCommas(property.price)} {property.currency}
+              </TableCell>
+              <TableCell>
+                <Badge>Active</Badge>
+              </TableCell>
+              <TableCell className="text-right space-x-2">
+                <Button size="icon" asChild>
+                  <Link
+                    href={`/user/dashboard/my-property/edit/${property._id}`}
+                  >
+                    <FaRegEdit size={16} />
+                  </Link>
+                </Button>
+                <Button size="icon" variant="destructive">
+                  <FaRegTrashAlt size={16} />
+                </Button>
+              </TableCell>
+            </TableRow>
+          );
+        })}
         {properties.length === 0 && (
           <TableRow>
             <TableCell colSpan={8} className="text-center">
