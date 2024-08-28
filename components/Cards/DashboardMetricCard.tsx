571273e2cb69117419d323cc5918d3d15993abde
Jahid0178 <jahidulhaque767@gmail.com>
2024-08-29 02:14:19 +0600

    feat(metric-card): add subTitle, status, and showStatus props to DashboardMetricCard
    - Enhanced the `DashboardMetricCard` component by introducing `subTitle`, `status`, and `showStatus` props.
    - `subTitle` now allows for an additional line of descriptive text below the title.
    - `status` and `showStatus` enable conditional rendering of the status label (Active/Deactive) based on the boolean status.
    - The `showStatus` prop determines whether to display the status or the count.

--- components/Cards/DashboardMetricCard.tsx
+++ components/Cards/DashboardMetricCard.tsx
@@ -3,23 +3,43 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 type PropertyStatsCardProps = {
   title: string;
-  count: number;
+  subTitle?: string;
+  count?: number;
+  status?: boolean;
+  showStatus?: boolean;
   icon: any;
 };
 
 const DashboardMetricCard = ({
   title,
+  subTitle,
   count,
+  status,
+  showStatus = false,
   icon,
 }: PropertyStatsCardProps) => {
+  console.log("status", status);
   return (
     <Card>
       <CardHeader className="flex-row justify-between items-center space-y-0">
-        <CardTitle>{title}</CardTitle>
+        <span className="space-y-1">
+          <CardTitle>{title}</CardTitle>
+          {subTitle && (
+            <p className="text-sm text-muted-foreground">{subTitle}</p>
+          )}
+        </span>
         <span>{icon}</span>
       </CardHeader>
       <CardContent>
-        <p className="text-3xl font-semibold">{count}</p>
+        {showStatus ? (
+          <p
+            className={`text-xl font-semibold ${status ? "text-green-500" : "text-red-500"}`}
+          >
+            {status ? "Active" : "Deactive"}
+          </p>
+        ) : (
+          <p className="text-3xl font-semibold">{count}</p>
+        )}
       </CardContent>
     </Card>
   );
