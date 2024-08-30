2c4c8ccea1d6e65a29ff4f29ac54332245ab667d
Jahid0178 <jahidulhaque767@gmail.com>
2024-08-31 04:37:23 +0600

    refactor(ui): optimize conditional rendering for location details
    - Extracted the `hasActivePlan` condition into a variable for improved readability.
    - Simplified the JSX structure for rendering address details or the "Buy Credit" prompt based on the user's plan status.

--- app/(client)/properties/[propertyId]/_component/PropertyLocation.tsx
+++ app/(client)/properties/[propertyId]/_component/PropertyLocation.tsx
@@ -4,35 +4,47 @@ import React from "react";
 import Link from "next/link";
 import authStore from "@/store/authStore";
 import { Button } from "@/components/ui/button";
-import { USER_MINIMUM_CREDIT } from "@/constant/constant";
+
+type PropertyLocationProps = {
+  location: {
+    street: string;
+    city: string;
+    zipCode: string;
+    country: string;
+  };
+};
 
 const PropertyLocation = ({
   location: { street, city, zipCode, country },
-}: any) => {
+}: PropertyLocationProps) => {
   const { user } = authStore((state) => state);
+
+  const hasActivePlan = user?.currentPlan?.status;
 
   return (
     <div className="mt-4">
       <h3 className="mb-2 text-lg font-semibold">Property Address</h3>
-      {user?.credit >= USER_MINIMUM_CREDIT ? (
-        <div className="flex justify-between items-center">
-          <address>
-            Street: {street},<br /> City: {city},<br /> Zip Code: {zipCode},
-            <br />
-            Country: {country}
-          </address>
-          <Button asChild>
-            <Link href="/user/dashboard/buy-credit">Get Map Features</Link>
-          </Button>
-        </div>
-      ) : (
-        <div className="flex gap-2">
-          <p>You need to buy credit to see location details</p>
-          <Link href="#" className="text-orange-500 hover:underline">
-            Buy Credit
-          </Link>
-        </div>
-      )}
+      <div className="flex justify-between items-center">
+        {hasActivePlan ? (
+          <>
+            <address>
+              Street: {street},<br /> City: {city},<br /> Zip Code: {zipCode},
+              <br />
+              Country: {country}
+            </address>
+            <Button asChild>
+              <Link href="/user/dashboard/buy-credit">Get Map Features</Link>
+            </Button>
+          </>
+        ) : (
+          <div className="flex gap-2">
+            <p>You need to buy plan to see location details</p>
+            <Link href="#" className="text-orange-500 hover:underline">
+              Buy Plan
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
