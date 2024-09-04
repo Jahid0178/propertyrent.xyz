ef8638d64c56927fe39b0dbce1a55f05e76be26a
Jahid0178 <jahidulhaque767@gmail.com>
2024-09-05 03:12:09 +0600

    feat(profile-dropdown): implement dynamic profile dropdown menu with user avatar, navigation, and logout functionality
    - Created `ProfileDropdownMenu` component to display user avatar and dynamically render dropdown menu items.
    - Integrated `authStore` to fetch user details such as full name and avatar for display in the dropdown.
    - Implemented logout functionality within the dropdown menu, allowing users to sign out via a "Log out" button.
    - Passed dynamic menu items via the `dropdownMenus` prop for easy navigation.
    - Included fallback handling for missing avatar images and user names.

---
+++ components/common/ProfileDropdownMenu/ProfileDropdownMenu.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import authStore from "@/store/authStore";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+interface DropdownMenuProps {
+  id: string;
+  label: string;
+  href: string;
+}
+
+interface ProfileDropdownMenuProps {
+  dropdownMenus: DropdownMenuProps[];
+}
+
+const ProfileDropdownMenu = ({ dropdownMenus }: ProfileDropdownMenuProps) => {
+  const { user, logout } = authStore((state) => state);
+  const userFullName = user?.fullName;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage
+            src={user?.avatar?.url || ""}
+            alt={user?.fullName || ""}
+          />
+          <AvatarFallback>{user?.fullName?.slice(0, 1) || ""}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>{`${userFullName} Account`}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {dropdownMenus.map((dropdownMenu: DropdownMenuProps) => (
+          <DropdownMenuItem key={dropdownMenu.id}>
+            <Link href={`${dropdownMenu.href}`}>{dropdownMenu.label}</Link>
+          </DropdownMenuItem>
+        ))}
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => logout()}>Log out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ProfileDropdownMenu;
