b2309ae8381ca8abb1e8e31691410c0068a1fa30
Jahid0178 <jahidulhaque767@gmail.com>
2024-09-09 09:59:30 +0600

    fix: add loading file only for properties page

--- app/(client)/properties/loading.tsx
+++ app/(client)/properties/loading.tsx
@@ -1,43 +1,7 @@
-import React from "react";
-import { Skeleton } from "@/components/ui/skeleton";
-
-const loading = () => {
+export default function Loading() {
   return (
-    <div>
-      <div className="container">
-        <div className="flex gap-4 mt-8">
-          {Array.from({ length: 12 }).map((_, index) => (
-            <Skeleton className="h-5 w-full rounded-xl" key={index} />
-          ))}
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
-          {Array.from({ length: 12 }).map((_, index) => (
-            <SkeletonCard key={index} />
-          ))}
-        </div>
-      </div>
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="loader" />
     </div>
   );
-};
-
-export default loading;
-
-const SkeletonCard = () => {
-  return (
-    <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[208px] w-full rounded-xl" />
-      <div className="space-y-4">
-        <Skeleton className="h-4 w-full" />
-        <div className="flex justify-between gap-4">
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[250px]" />
-        </div>
-        <div className="flex justify-between gap-4">
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-full" />
-        </div>
-      </div>
-    </div>
-  );
-};
+}
