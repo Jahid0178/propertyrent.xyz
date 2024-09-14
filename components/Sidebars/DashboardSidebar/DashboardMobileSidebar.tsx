4883f3987280ac2249e99307d80cb0ebda161a3f
Jahid0178 <jahidulhaque767@gmail.com>
2024-09-14 15:05:46 +0600

    fix: fixed link query issue in dashboard sidebar

--- components/Sidebars/DashboardSidebar/DashboardMobileSidebar.tsx
+++ components/Sidebars/DashboardSidebar/DashboardMobileSidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { FaBarsStaggered } from "react-icons/fa6";
@@ -11,8 +13,15 @@ import {
 import { dashboardSidebarLinks } from "@/data/data";
 import Logo from "@/components/common/Logo/Logo";
 import Link from "next/link";
+import authStore from "@/store/authStore";
+import { UserRole } from "@/typescript/types";
 
 const DashboardMobileSidebar = () => {
+  const { user } = authStore((state) => state);
+
+  const userRole = user?.role as UserRole;
+
+  console.log("dashboardSidebarLinks", dashboardSidebarLinks);
   return (
     <Sheet>
       <SheetTrigger className="inline-block lg:hidden">
@@ -25,10 +34,10 @@ const DashboardMobileSidebar = () => {
           </SheetTitle>
         </SheetHeader>
         <ul className="space-y-4">
-          {dashboardSidebarLinks.map((link) => (
+          {dashboardSidebarLinks[userRole]?.map((link) => (
             <li key={link.id}>
               <Link
-                href={link.href}
+                href={link.query ? `${link.href}?id=${user?._id}` : link.href}
                 className="p-2 flex gap-2 items-center w-full"
               >
                 {link.icon({ size: 20 })} {link.name}
